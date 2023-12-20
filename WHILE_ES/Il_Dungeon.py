8d916fa08067c65d643b5a8193601d723dfcb251
Rohit <rohit.verma@itsrizzoli.it>
2023-12-20 09:02:33 +0100

    Fixed the "Il Dungeon"

--- WHILE_ES/Il_Dungeon.py
+++ WHILE_ES/Il_Dungeon.py
@@ -5,31 +5,36 @@ if __name__ == '__main__':
     exit = True
     print("Entro")
     i = 0
-    found = ""
+    founded = []
     while exit:
         if i < len(caverna):
             if cerco == caverna[i]:
                 print(f"Che fortuna! Ho trovato {cerco}")
-                found = cerco
                 exit = False
             else:
                 print("Vedo", caverna[i])
+                founded.append(caverna[i])
             i += 1
         else:
             print(f"Purtroppo {cerco} non c'è")
             exit = False
 
-    while i >= 0:
-        if cerco == found:
-            print("Torno indietro")
-            while i > 0:
-                i -= 1
-                print("Vedo", caverna[i])
-            print("Esco")
-        else:
-            print("Torno indietro")
-            while i > 0:
-                i -= 1
-                print("Vedo", caverna[i])
-            print("Esco")
-        i -= 1
+    print("Torno indietro")
+    for i in founded[::-1]:
+        print("Vedo", i)
+    print("Esco")
+
+    # while i >= 0:
+    #     if cerco == found:
+    #         print("Torno indietro")
+    #         while i > 0:
+    #             i -= 1
+    #             print("Vedo", caverna[i])
+    #         print("Esco")
+    #     else:
+    #         print("Torno indietro")
+    #         while i > 0:
+    #             i -= 1
+    #             print("Vedo", caverna[i])
+    #         print("Esco")
+    #     i -= 1
